b87956bbd8ae9d4b4147c4f51dd0d5276b91ac93
FrancoMarengo <francomarengo.21@gmail.com>
2026-04-30 15:55:33 -0300

    Resuelvo el issue de duplicacion al escanear codigo de barras, agregando nuevas páginas para confirmacion de colision

--- app/(app)/camera.styles.ts
+++ app/(app)/camera.styles.ts
@@ -127,12 +127,4 @@ export const styles = StyleSheet.create({
     height: 120,
     borderRadius: 8,
   },
-  loadingOverlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'rgba(0,0,0,0.5)',
-  },
 });
